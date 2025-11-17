e1289b4097386784456d30e73ea76fde06f6c90d
Duarte <d.j.fernandes.da.silva@tue.nl>
2025-11-17 14:04:01 +0100

    bump version in docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Imodulator'
 copyright = '2025, Duarte Silva, Kaan Sünnetçioğlu'
 author = 'Duarte Silva, Kaan Sünnetçioğlu'
-release = '0.0.0'
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
